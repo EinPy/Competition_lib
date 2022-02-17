5905795c7c9bc7223663467f392523b64a9cbeba
Einar <einar.bratthall@gmail.com>
2022-02-17 20:01:51 +0100

    solved dp problem

--- 30DaysOfCodeforces/10_Boredom.py
+++ 30DaysOfCodeforces/10_Boredom.py
@@ -10,7 +10,7 @@ def setupInputOutputSublime():
     if os.name == 'nt':
         sys.stdout = open("C:\\Temp\\output.txt", mode = 'w')
         sys.stdin = open("C:\\Temp\\input.txt", mode = 'r')
-setupInputOutputSublime()
+#setupInputOutputSublime()
 
 
 itr = (line for line in sys.stdin.read().strip().split('\n'))
@@ -18,12 +18,38 @@ input = lambda: next(itr)
 
 #https://codeforces.com/problemset/problem/455/A
 
-n = int(input)
-a = list(map(int,input().split()))
-
 # not for today, look at this https://bruceoutdoors.wordpress.com/2015/11/08/445a-boredom-codeforces-tutorial/
-dp = [0 for _ in range(n) + 1]
-def solve():
-    if len(a) == 0:
-        return
 
+
+def solve(m):
+    dp[0],dp[1] = 0, c[1]
+    
+    for i in range(2,m+1):
+        dp[i] = max(dp[i-1],dp[i-2]+i*c[i])
+
+    return dp[m]
+
+
+n = int(input())
+a = list(map(int,input().split()))
+a.sort()
+c = [0 for _ in range(1000001)]
+m = 0
+dp = [None for _ in range(1000001)]
+for v in a:
+    c[v] += 1
+    m = max(m, v)
+
+print(solve(m))
+
+'''
+#case 2
+max(x.count(1)*1,x.count(2)*2)
+#case 3
+max(x.count(1)*1 + x.count(3)*3,x.count(2)*2)
+
+when on a number you can reduce it to two cases if you
+start at the max
++ f(i-2) + f(i)
++ f(i-1) 
+'''
