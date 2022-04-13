ee32bb100348cbdf9fa005033a7aadea4b394d99
Einar Bratthall <einar.bratthall@gmail.com>
2022-04-13 16:07:23 +0200

    leetcode dp problem

--- 30DaysOfCode/25_BuyAndSellStockOneAndTwo.py
+++ 30DaysOfCode/25_BuyAndSellStockOneAndTwo.py
@@ -1,3 +1,5 @@
+#solution to https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+
 def maxProfit(prices):
     curMin, curMax = 10**6, 0
     bestDiff = 0
@@ -12,3 +14,22 @@ def maxProfit(prices):
             bestDiff =  max(bestDiff,curMax - curMin)
     
     return bestDiff
+
+
+def maxProfit2(prices):       
+    profit = 0
+    buyPrice = -1
+    
+    for i in range(len(prices)-1):
+        if prices[i+1] < prices[i]:
+            if buyPrice != -1:
+                profit += prices[i] - buyPrice
+                buyPrice = -1
+        if prices[i+1] > prices [i]:
+            if buyPrice == -1:
+                buyPrice = prices[i]
+                
+    if buyPrice != -1:
+        profit += prices[-1] - buyPrice
+                
+    return profit
